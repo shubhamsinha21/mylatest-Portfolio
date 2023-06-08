101b1feb55654978e7d495ed3f0914c87cb5776b
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-06-08 15:26:23 +0530

    created constans & app.jsx changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 const App = () => {
-  return <div>3D DEVELOPER PORTFOLIO</div>;
+  return (
+    <>
+      {/* <div>hrll</div> */}
+      <BrowserRouter>
+        <div className="relative z-0 bg-tertiary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import styles from "../style";
+import navLink from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    ></nav>
+  );
+};
 
-export default Navbar
+export default Navbar;

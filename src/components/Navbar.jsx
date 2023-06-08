5b77925f0c95d1d088f28ba70d4a0b5199dfb012
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-06-08 17:59:34 +0530

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import styles from "../style";
-import navLink from "../constants";
+import { styles } from "../style";
+import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
-    ></nav>
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white-[18px] font-bold cursor-pointer">
+            Shubham <span className="sm:block hidden">Sinha </span>
+          </p>
+          <p className="text-white-[18px] font-bold cursor-pointer">
+            Shubham <span className="sm:block hidden">Sinha </span>
+          </p>
+        </Link>
+      </div>
+    </nav>
   );
 };
 

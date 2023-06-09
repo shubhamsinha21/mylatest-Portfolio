206a73421848edd71846e90080980099d4d5db7a
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-06-09 17:11:30 +0530

    some doubts cleared

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
     <div>Hero</div>
-  )
-}
+    // <div className="relative w-full h-screen mx-auto">
+    //   {/* <div
+    // className={`${styles.paddingX} absolute inset-0 top-[120px]
+    // max-w-7xl mx-auto flex flex-auto items-start gap-5`}
+    //   >
 
-export default Hero
+    //   </div> */}
+    // </div>
+  );
+};
+
+export default Hero;
